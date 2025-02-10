fe4da25e62675ff0c39403140802fbd41149f1ed
Clint <clinthiles1992@gmail.com>
2025-02-10 11:15:19 -0800

    Enhance Hero component with updated content and styling for improved readability

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import '../components/Header.css';
+import React from "react";
+import "./Hero.css";
 
 function Hero() {
   return (
-    <section id='hero' className='hero section dark-background'>
+    <section id="hero" className="hero section dark-background">
       <div
-        id='hero-carousel'
-        className='carousel slide carousel-fade'
-        data-bs-ride='carousel'
-        data-bs-interval='5000'
+        id="hero-carousel"
+        className="carousel slide carousel-fade"
+        data-bs-ride="carousel"
+        data-bs-interval="5000"
       >
-        <div className='carousel-items'>
-          <img
-            src='assets/img/hero_1.jpg'
-            style={{ filter: 'brightness(0.4)' }}
-            alt=''
-          />
-          <div className='carousel-container'>
+        <div className="carousel-items">
+          <img src="assets/img/hero_1.jpg" style={{ filter: "brightness(0.4)" }} alt="" />
+          <div className="carousel-container">
             <h2>The Wyatt Wiebe Centre For Healing</h2>
-            <p>
-              Located in Sturgeon County, Alberta a quick thirty minutes from
-              Edmonton, the centre is situated on 84 acres. We are home to large
-              green spaces, a riding arena, and multiple healing spaces
-              including an indoor pool, massage/energy healing room, and so much
-              more.
+            <p className="intro-text">
+              Located in Sturgeon County, Alberta a quick thirty minutes from Edmonton, The Wyatt
+              Wiebe Centre for Healing is situated on 84 acres of land in rural Alberta. We are the
+              home to a large riding arena, four beautiful horses, a pool lovingly named Joy, a
+              large art studio, and vast green spaces. We have multiple session rooms fit for energy
+              healing, card reading, coaching, meditation, yoga classes, and so much more. We have
+              hosted healing sessions, creative workshops, and other events, and we plan on hosting
+              many more in the future. We hope to be a space to gather and connect to create a sense
+              of community.
             </p>
           </div>
         </div>
