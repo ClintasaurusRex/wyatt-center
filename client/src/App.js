08642fdd1ef1fea92b9cf7c15a8706dcf5f4d56b
Clint <clinthiles1992@gmail.com>
2025-02-11 10:46:24 -0800

    Refactor code style for consistency and add Gallery route to navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,33 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import OurStory from './components/OurStory';
-import About from './pages/About';
-import Services from './pages/Services';
-import Footer from './components/Footer';
-import Contact from './pages/Contact';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import OurStory from "./components/OurStory";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Footer from "./components/Footer";
+import Contact from "./pages/Contact";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className='index-page'>
+      <div className="index-page">
         <Header />
         <Routes>
           <Route
-            path='/'
+            path="/"
             element={
-              <main className='main'>
+              <main className="main">
                 <Hero />
                 <OurStory />
               </main>
             }
           />
-          <Route path='/about' element={<About />} />
-          <Route path='/services' element={<Services />} />
-          <Route path='/contact' element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/gallery" element=<div>Gallery Here</div> />
         </Routes>
         <Footer />
       </div>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,71 +1,65 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import '../styles/Header.css';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import "../styles/Header.css";
 
 function Header() {
   const location = useLocation();
 
   return (
-    <header
-      id='header'
-      className='header d-flex align-items-center position-relative'
-    >
-      <div className='container-fluid container-xl position-relative d-flex align-items-center justify-content-between'>
-        <Link to='/' className='logo d-flex align-items-center'>
-          <img src='assets/img/wwch.png' alt='AgriCulture' />
-          <div className='logo-text'>
-            <p id='logo-text'>
+    <header id="header" className="header d-flex align-items-center position-relative">
+      <div className="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+        <Link to="/" className="logo d-flex align-items-center">
+          <img src="assets/img/wwch.png" alt="AgriCulture" />
+          <div className="logo-text">
+            <p id="logo-text">
               The Wyatt Wiebe <br />
               Centre For Healing
             </p>
           </div>
         </Link>
 
-        <nav id='navmenu' className='navmenu'>
+        <nav id="navmenu" className="navmenu">
           <ul>
             <li>
-              <Link
-                to='/'
-                className={`nav-link ${
-                  location.pathname === '/' ? 'active' : ''
-                }`}
-              >
+              <Link to="/" className={`nav-link ${location.pathname === "/" ? "active" : ""}`}>
                 Home
               </Link>
             </li>
             <li>
               <Link
-                to='/about'
-                className={`nav-link ${
-                  location.pathname === '/about' ? 'active' : ''
-                }`}
+                to="/about"
+                className={`nav-link ${location.pathname === "/about" ? "active" : ""}`}
               >
                 About Us
               </Link>
             </li>
             <li>
               <Link
-                to='/services'
-                className={`nav-link ${
-                  location.pathname === '/services' ? 'active' : ''
-                }`}
+                to="/services"
+                className={`nav-link ${location.pathname === "/services" ? "active" : ""}`}
               >
                 Our Services
               </Link>
             </li>
             <li>
               <Link
-                id='contact-link'
-                to='/contact'
-                className={`nav-link ${
-                  location.pathname === '/contact' ? 'active' : ''
-                }`}
+                to="/gallery"
+                className={`nav-link ${location.pathname === "/gallery" ? "active" : ""}`}
+              >
+                Gallery
+              </Link>
+            </li>
+            <li>
+              <Link
+                id="contact-link"
+                to="/contact"
+                className={`nav-link ${location.pathname === "/contact" ? "active" : ""}`}
               >
                 Contact
               </Link>
             </li>
           </ul>
-          <i className='mobile-nav-toggle d-xl-none bi bi-list'></i>
+          <i className="mobile-nav-toggle d-xl-none bi bi-list"></i>
         </nav>
       </div>
     </header>
