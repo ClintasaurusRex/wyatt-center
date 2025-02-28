aed8c8cec41aadf22d295b61d8f73462e08d3318
Clint <clinthiles1992@gmail.com>
2025-02-28 10:07:05 -0800

    Re set up navBar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,24 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Header.css";
 import NavLinks from "./NavLinks";
-
 function Header() {
   return (
-    <header id="header" className="header d-flex align-items-center position-relative">
-      <div className="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
-        <Link to="/" className="logo d-flex align-items-center">
-          <img src="assets/img/wwch.png" alt="AgriCulture" />
-          <div className="logo-text">
-            <p id="logo-text">
-              The Wyatt Wiebe <br />
-              Centre For Healing
-            </p>
-          </div>
-        </Link>
+    <header id="header" className="header d-flex position-relative">
+      <div className="container-fluid container-xl position-relative d-flex justify-content-between align-items-center">
+        <div className="logo-container ">
+          <Link to="/" className="logo d-flex align-items-center">
+            <img src="assets/img/wwch.png" alt="AgriCulture" />
+            <div className="logo-text"></div>
+          </Link>
+          <p id="logo-text">
+            The Wyatt Wiebe <br />
+            Centre For Healing
+          </p>
+        </div>
         <NavLinks />
       </div>
     </header>
   );
 }
-
 export default Header;
